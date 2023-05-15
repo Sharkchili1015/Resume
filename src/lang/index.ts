03b270a467bea2ed8ca488d62c02a72aee256092
Ricky <1372896443@qq.com>
2023-05-15 21:43:39 +0800

    feat:编译时动态替换简历人

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -1,18 +1,16 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import RICKY from "./locales/ricky";
-import LSX from './locales/lsx'
 import getEnvConfig from "../utils/config";
 
 const { VITE_RESUME_OWNER } = getEnvConfig();
 
 i18n.use(initReactI18next).init({
   resources: {
-    RICKY: { translation: RICKY },
-    LSX: { translation: LSX },
+    RICKY: { translation: RICKY }
   },
   fallbackLng: VITE_RESUME_OWNER,
-  preload: ["RICKY","LSX"], //在这里添加你们的locales表
+  preload: ["RICKY"], //在这里添加你们的locales表
   interpolation: {
     escapeValue: false,
   },
--- src/lang/locales/lsx.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    userInfo:{
-        name:'炼奶'
-    }
-}
-  
