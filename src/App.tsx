d006239e6d3b8170ef58b4bd8c3914050e74c929
Ricky <1372896443@qq.com>
2023-05-28 02:08:36 +0800

    feat:Eslint Config

--- src/App.tsx
+++ src/App.tsx
@@ -8,31 +8,36 @@ function App() {
   const { t } = useTranslation();
   const [count, setCount] = useState(0);
 
-  const handleClick = (count) => { 
+  const handleClick = () => {
     setCount((count) => count + 1);
   };
 
   return (
-    <>
-      <div className="flex justify-center">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>目前简历模板为：{t("userInfo.name")}</h1>
-      <div className="card">
-        <button onClick={() => handleClick(count)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+	<>
+		<div className="flex justify-center">
+			<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+				<img src={viteLogo} className="logo" alt="Vite logo" />
+			</a>
+
+			<a href="https://react.dev" target="_blank" rel="noreferrer">
+				<img src={reactLogo} className="logo react" alt="React logo" />
+			</a>
+		</div>
+
+		<h1>目前简历模板为：{t("userInfo.name")}</h1>
+
+		<div className="card">
+			<button onClick={() => handleClick()}>count is {count}</button>
+
+			<p>
+				Edit <code>src/App.tsx</code> and save to test HMR
+			</p>
+		</div>
+
+		<p className="read-the-docs">
+			Click on the Vite and React logos to learn more
+		</p>
+	</>
   );
 }
 
