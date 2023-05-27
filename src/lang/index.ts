140e1e5db8cf167ed1545571d7043c6c0c547075
coldJayra <846803233@qq.com>
2023-05-27 15:19:17 +0800

    1

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -1,16 +1,16 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import RICKY from "./locales/ricky";
+import COLDJAYRA from "./locales/coldjayra";
 import getEnvConfig from "../utils/config";
 
 const { VITE_RESUME_OWNER } = getEnvConfig();
 
 i18n.use(initReactI18next).init({
   resources: {
-    RICKY: { translation: RICKY }
+    COLDJAYRA: { translation: COLDJAYRA }
   },
   fallbackLng: VITE_RESUME_OWNER,
-  preload: ["RICKY"], //在这里添加你们的locales表
+  preload: ["COLDJAYRA"], //在这里添加你们的locales表
   interpolation: {
     escapeValue: false,
   },
---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <>
+        <h1>目前简历模板为：{ t("userInfo.name") } </h1>
+            < />
+    )
+}
+
+export default Home;
